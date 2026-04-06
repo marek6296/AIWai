db35fb0fe4cc74237b285379e323697bbd1a0360
Marek <cmelo.marek@gmail.com>
2026-04-06 15:51:23 +0200

    Design: Remove unnecessary WhyUs (Methodology) section to ensure a leaner and cleaner page flow

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
     <main className="min-h-screen bg-white">
       <HeroSection />
       <ServicesSection />
-      <WhyUsSection />
       <CTASection />
       <ContactSection />
       <NewsSection />
